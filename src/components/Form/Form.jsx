c4fc6ae0dccd799f5239030adb6ea854389eba55
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-03 20:50:12 -0400

    Add and set states for each form input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import FormInput from '../FormInput/FormInput'
 
 const Form = () => {
+  //Set States
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
   return (
     <div className="app">
         <form>
-            <FormInput placeholder="username" />
-            <FormInput placeholder="name" />
-            <FormInput placeholder="email"/>
-            <FormInput placeholder="password"/>
-            <FormInput placeholder="password again"/>
+            <FormInput placeholder="username" setUsername/>
+            <FormInput placeholder="name" setName/>
+            <FormInput placeholder="email" setEmail/>
+            <FormInput placeholder="password" setPassword/>
+            <FormInput placeholder="password again" setVerifyPassword/>
         </form>
     </div>
   )
