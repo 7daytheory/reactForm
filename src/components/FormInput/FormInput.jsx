33968ff891a4bfeef9589bc254cdd6ee669c86e1
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-03 20:37:36 -0400

    Added form and input components, import to App

---
+++ src/components/FormInput/FormInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FormInput.css';
+
+const FormInput = () => {
+  return (
+    <div className="formInput">
+        <label>Username</label>
+        <input />
+    </div>
+  )
+}
+
+export default FormInput
